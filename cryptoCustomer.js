134a4a21f24973231a2f59863bb206af11f29fbe
Brian Lee <bklcross@gmail.com>
2018-02-10 11:47:24 -0800

    i think i broke it

--- cryptoCustomer.js
+++ cryptoCustomer.js
@@ -16,26 +16,24 @@ var connection = mysql.createConnection(
 connection.connect(function(err)
 {
   if (err) {console.log(err);}
-
-  showCrypto();
-
-  questions();
-
+  showCrypto(questions);
   connection.end();
-
 });
 
-function showCrypto()
+function showCrypto(CallBack)
 {
     console.log("Welcome to the bkl_EXCHANGE\n");
     connection.query("SELECT * FROM cryptos", function(err, res)
     {
       if (err) throw err;
       console.table(res);
+      CallBack();
     });
+    
+
 }
 
-function questions()
+function questions(CallBack)
 {
   inquirer.prompt([
   {
@@ -46,29 +44,53 @@ function questions()
   {
     name: "quant",
     type: "input",
-    message: "How many coins should you like purchase?"
+    message: "How many coins would you like purchase?"
   }
-  ]).then(answers =>
+  ]).then(function(answers)
   {
-    console.log("Item:", answers.itemNum);
-    console.log("Quantity:", answers.quant);
-    connection.query("SELECT circulating_supply FROM cryptos WHERE item_id = ?", [answers.itemNum], function(err, res, fields)
+    var connectQuery1 = "SELECT * FROM cryptos WHERE item_id="+ answers.itemNum
+    connection.query(connectQuery1, function(err, res)
     {
-      if (err) throw err
-      else
+      if (answers.quant > res[0].circulating_supply)
       {
-        if (answers.itemNum > 10)
-        {
-          console.log("That crypto is not included in this exchange");
-        }
-        else
-        {
-          if (answers.quant > res.circulating_supply)
-          {
-            console.log("Sorry the exchange doesn't have enough");
-          }
-        }
+        console.log("Sorry the exchange doesn't have enough");
       }
-    });
+    })
+    CallBack();
+  })
+}
+
+function update()
+{ 
+  var Quantity = answers.quant;
+  var Item = answers.itemNum;
+  var connectQuery = "UPDATE cryptos SET circulating_supply = circulating_supply - " + Quantity + " WHERE item_id = " + Item;
+  var current_circulating_supply = 0;
+
+  console.log("Item:", answers.itemNum);
+  console.log("Quantity:", answers.quant);
+
+  connection.query(connectQuery, function(err, res)
+  {
+    if (err) throw err
+      
+    else
+      {
+        console.log("Your order has been processed");
+        getCirculatingSupply(Quantity);
+      }
   });
 }
+
+function getCirculatingSupply(quantity)
+{
+  connection.query("SELECT circulating_supply FROM cryptos WHERE ?", [{ item_id: quantity}], function(err, res)
+  {
+    if (err) throw err;
+    else {
+      current_circulating_supply = res[0].circulating_supply;
+      console.log("Current Circulating Supply:", current_circulating_supply);
+    }
+  });
+};
+

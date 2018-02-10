60885dc6cba37de206564547eb5c8fb83b165287
Brian Lee <bklcross@gmail.com>
2018-02-09 22:29:25 -0800

    interest attempt

--- cryptoCustomer.js
+++ cryptoCustomer.js
@@ -2,7 +2,6 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
 
-
 var connection = mysql.createConnection(
 {
   host: "localhost",
@@ -14,42 +13,62 @@ var connection = mysql.createConnection(
   database: "bkl_exchange"
 });
 
+connection.connect(function(err)
+{
+  if (err) {console.log(err);}
+
+  showCrypto();
+
+  questions();
+
+  connection.end();
+
+});
+
 function showCrypto()
 {
     console.log("Welcome to the bkl_EXCHANGE\n");
     connection.query("SELECT * FROM cryptos", function(err, res)
     {
-      if (err)
-      {
-      console.log(err);    
-      }
+      if (err) throw err;
       console.table(res);
-      connection.end();
     });
 }
 
-connection.connect(
-function(err)
+function questions()
 {
-  if (err)
+  inquirer.prompt([
   {
-    console.log(err);
+    name: "itemNum",
+    type: "input",
+    message: "Which crypto do you want to buy?"
+  },
+  {
+    name: "quant",
+    type: "input",
+    message: "How many coins should you like purchase?"
   }
-  showCrypto();
-
-  function questions (params)
+  ]).then(answers =>
   {
-    inquirer.prompt(
-    [{
-      name: "item_number",
-      type: "input",
-      message: "Please input item_id of the crypto you want."
-    }])
-    .then(answers =>
+    console.log("Item:", answers.itemNum);
+    console.log("Quantity:", answers.quant);
+    connection.query("SELECT circulating_supply FROM cryptos WHERE item_id = ?", [answers.itemNum], function(err, res, fields)
     {
-      console.log(answers);
+      if (err) throw err
+      else
+      {
+        if (answers.itemNum > 10)
+        {
+          console.log("That crypto is not included in this exchange");
+        }
+        else
+        {
+          if (answers.quant > res.circulating_supply)
+          {
+            console.log("Sorry the exchange doesn't have enough");
+          }
+        }
+      }
     });
-  }
-});
-
-
+  });
+}
